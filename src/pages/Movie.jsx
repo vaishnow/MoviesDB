a240a0e1dadb0f9aef7f32a81a3cebee5fcdf56d
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-02-15 22:07:15 +0530

    added ToastContainer to Movie page

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import MoviesMain from "../components/MoviesMain";
 import MoviesCrew from "../components/MoviesCrew";
 import Videos from "../components/Videos";
 import { discoverContent } from "../api/tmdb";
 import "./Movie.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function Movie({ content }) {
   const [movieDetails, setMovieDetails] = useState({});
@@ -31,6 +33,15 @@ function Movie({ content }) {
       <MoviesCrew crew={movieDetails.credits?.cast} title={"Actors"} />
       <MoviesCrew crew={movieDetails.credits?.crew} title={"Crew Members"} />
       <Videos videos={movieDetails.videos?.results} />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        limit={2}
+        hideProgressBar
+        newestOnTop
+        closeOnClick={false}
+        theme="dark"
+      />
     </div>
   );
 }
