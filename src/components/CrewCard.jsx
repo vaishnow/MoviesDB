062a0a00c83d44b878f1fe2048882b4f6a487964
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-03-19 12:03:03 +0530

    update Crew component to have alternate image

--- src/components/CrewCard.jsx
+++ src/components/CrewCard.jsx
@@ -1,27 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import { Avatar } from "@mui/material";
 import "./CrewCard.css";
 
 const CrewCard = ({ member }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div className="crew-card m-2">
-      <img
-        src={
-          member.profile_path &&
-          `https://image.tmdb.org/t/p/w400/${member.profile_path}`
-        }
-        alt=""
-		loading="lazy"
-      />
-      <div
-        className={
-          (member.department ? "h-20 pt-6" : "h-16 pt-8") +
-          " -translate-y-full rounded-b text-center text-nowrap text-ellipsis overflow-hidden bw-gradient"
-        }
-      >
+      {isLoading && (
+        <Avatar variant="rounded" sx={{ width: "100%", height: "100%" }} />
+      )}
+      {member.profile_path && (
+        <img
+          src={`https://image.tmdb.org/t/p/w400/${member.profile_path}`}
+          alt=""
+          loading="lazy"
+          onLoad={() => setIsLoading(false)}
+          className={isLoading ? "w-0 h-0" : "w-full h-full"}
+        />
+      )}
+      <div className="h-20 pt-6 -translate-y-full rounded-b text-center text-nowrap text-ellipsis overflow-hidden bw-gradient">
         <span className="block">{member.name}</span>
-        {member.department && (
-          <span className="text-sm text-gray-400">{member.department}</span>
-        )}
+        <span className="text-sm text-gray-400">
+          {member.character || member.department}
+        </span>
       </div>
     </div>
   );
