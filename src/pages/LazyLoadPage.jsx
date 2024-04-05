f071c70178e1ba113551943869d3f50098ccd89d
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-04-05 14:01:58 +0530

    redesign LazyLoadPage

--- src/pages/LazyLoadPage.jsx
+++ src/pages/LazyLoadPage.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import "./LazyLoadPage.css";
 
 function LazyLoadPage() {
   return (
-    <div className="mdb-page py-40 sm:p-4 text-center">
-      <h1 className="text-8xl my-36">Loading...</h1>
-      <h4>is your internet allright ?</h4>
+    <div className="mdb-page pt-64 pb-96 h-dvh sm:px-4 text-center">
+      <div className="spinner"></div>
+      <h4 className="text-2xl font-bold mt-52 glow">Loading<span className="glow">...</span></h4>
     </div>
   );
 }
